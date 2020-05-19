e37265735abd953604aec876a17f014f34061295
Monika Stanojkovikj <monika.stanojkovic2@gmail.com>
2020-05-19 15:28:09 +0200

    Upload HW26_02

---
+++ Homework26_MonikaStanojkovikj/visit_counter/index.php
@@ -0,0 +1,31 @@
+<?php
+//start session
+session_start();
+    if(isset($_SESSION['counter'])){
+        $_SESSION['counter'] = $_SESSION['counter'] + 1;
+    }
+    else{
+        $_SESSION['counter'] =1;
+    }
+
+//if session variable 'counter' not exist create it and set to 1, if it exist increase it by 1
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Visit counter</title>
+</head>
+<body>
+    <h1>Visit counter</h1>
+    <p>You opened this page: </p>
+    <?php 
+    if(isset($_SESSION['counter'])) {
+        echo "<h3>".$_SESSION['counter']." times.</h3>";
+    }  
+    ?>
+        
+</body>
+</html>
